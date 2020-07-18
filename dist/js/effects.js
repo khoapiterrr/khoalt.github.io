77f011c6a8414888913b591d95ce25b89a557bab
khoapiterrr <ltk.gym@gmail.com>
2020-07-18 16:20:19 +0700

    animation section

--- dist/js/effects.js
+++ dist/js/effects.js
@@ -1,66 +1,84 @@
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-    console.log('tớ xin log');
-    const e = document.getElementById('btnSideBar'),
-      t = document.getElementById('page'),
-      o = document.getElementById('overlay'),
-      l = document.getElementById('navRight'),
-      n = document.getElementById('crtSidebarClose'),
-      d = document.getElementById('aboutMe'),
-      c = document.getElementById('skills'),
-      s = document.getElementById('education'),
-      i = document.getElementById('interested'),
-      m = document.getElementById('btnAboutme'),
-      a = document.getElementById('btnSkill'),
-      r = document.getElementById('btnEducation'),
-      y = document.getElementById('btnLove'),
-      u = document.getElementById('scroolToTop');
-    (m.onclick = () => {
-      d.scrollIntoView();
-    }),
-      (a.onclick = () => {
-        c.scrollIntoView();
-      }),
-      (r.onclick = () => {
-        s.scrollIntoView();
-      }),
-      (y.onclick = () => {
-        i.scrollIntoView();
-      }),
-      (u.onclick = () => {
-        u.classList.add('hidden-scrool');
-      }),
-      (e.onclick = () => {
-        console.log('btnSideBar click'),
-          t.classList.add('hidden-scroll'),
-          o.classList.remove('hidden-overlay'),
-          l.classList.remove('nav-right-hidden');
-      }),
-      (o.onclick = () => {
-        t.classList.remove('hidden-scroll'),
-          o.classList.add('hidden-overlay'),
-          l.classList.add('nav-right-hidden');
-      }),
-      (n.onclick = o.onclick),
-      (document.getElementById('scroolToTop').onclick = () => {
-        u.classList.add('hidden-scroll'),
-          u.classList.remove('visibility-scroll'),
-          setTimeout(() => (u.style.display = 'none'), 600),
-          document.body.scrollIntoView();
-      });
+    const btnSideBar = document.getElementById('btnSideBar'),
+      page = document.getElementById('page'),
+      overlay = document.getElementById('overlay'),
+      navRight = document.getElementById('navRight'),
+      crtSidebarClose = document.getElementById('crtSidebarClose');
+    const aboutMe = document.getElementById('aboutMe'),
+      skills = document.getElementById('skills'),
+      education = document.getElementById('education'),
+      interested = document.getElementById('interested'),
+      btnAboutme = document.getElementById('btnAboutme'),
+      btnSkill = document.getElementById('btnSkill'),
+      btnEducation = document.getElementById('btnEducation'),
+      btnLove = document.getElementById('btnLove'),
+      scroolToTop = document.getElementById('scroolToTop');
+    const sectionPage = [aboutMe, skills, education, interested];
+    // scroll to div
+    btnLove.onclick = () => {
+      interested.scrollIntoView();
+    };
+    btnEducation.onclick = () => {
+      education.scrollIntoView();
+    };
+    btnSkill.onclick = () => {
+      skills.scrollIntoView();
+    };
+    btnAboutme.onclick = () => {
+      aboutMe.scrollIntoView();
+    };
+
+    btnSideBar.onclick = () => {
+      page.classList.add('hidden-scroll'),
+        overlay.classList.remove('hidden-overlay'),
+        navRight.classList.remove('nav-right-hidden');
+    };
+    overlay.onclick = () => {
+      page.classList.remove('hidden-scroll'),
+        overlay.classList.add('hidden-overlay'),
+        navRight.classList.add('nav-right-hidden');
+    };
+    crtSidebarClose.onclick = overlay.onclick;
+    document.getElementById('scroolToTop').onclick = () => {
+      scroolToTop.classList.add('hidden-scroll'),
+        scroolToTop.classList.remove('visibility-scroll'),
+        setTimeout(() => (scroolToTop.style.display = 'none'), 600),
+        document.body.scrollIntoView();
+    };
+    for (const item of sectionPage) {
+      if (
+        !checkExitedAnimated(item) &&
+        window.scrollY + screen.height > item.offsetTop
+      ) {
+        item.classList.add('section-animated');
+        item.classList.remove('section-animate');
+      }
+    }
+    window.onscroll = () => {
+      const crollCurrent = window.scrollY,
+        heighScroll = document.body.scrollHeight;
+      crollCurrent > 0.1 * heighScroll
+        ? (scroolToTop.classList.add('visibility-scroll'),
+          scroolToTop.classList.remove('hidden-scroll'),
+          setTimeout(() => (scroolToTop.style.display = 'block'), 600))
+        : (scroolToTop.classList.add('hidden-scroll'),
+          scroolToTop.classList.remove('visibility-scroll'),
+          setTimeout(() => (scroolToTop.style.display = 'none'), 600));
+
+      for (const item of sectionPage) {
+        if (
+          !checkExitedAnimated(item) &&
+          crollCurrent + screen.height > item.offsetTop
+        ) {
+          item.classList.add('section-animated');
+          item.classList.remove('section-animate');
+        }
+      }
+    };
   },
-  !1,
-),
-  (window.onscroll = () => {
-    const e = document.documentElement.scrollTop,
-      t = document.body.scrollHeight,
-      o = document.getElementById('scroolToTop');
-    e > 0.1 * t
-      ? (o.classList.add('visibility-scroll'),
-        o.classList.remove('hidden-scroll'),
-        setTimeout(() => (o.style.display = 'block'), 600))
-      : (o.classList.add('hidden-scroll'),
-        o.classList.remove('visibility-scroll'),
-        setTimeout(() => (o.style.display = 'none'), 600));
-  });
+  false,
+);
+
+const checkExitedAnimated = (e) => e.classList.contains('section-animated');
